a68a8f0bb827cd98e117aaf6f5ddc619fcf8e56b
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-04-19 21:57:36 +0200

    Crear modelos

---
+++ backend/reservas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import ClaseYoga, Reserva
+
+admin.site.register(ClaseYoga)
+admin.site.register(Reserva)
+
+
+# Register your models here.
---
+++ backend/reservas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class ClaseYoga(models.Model):
+    nombre = models.CharField(max_length=100)
+    dia = models.CharField(max_length=20)
+    hora = models.TimeField()
+    cupo_maximo = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} - {self.dia} a las {self.hora}"
+
+class Reserva(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    clase = models.ForeignKey(ClaseYoga, on_delete=models.CASCADE)
+    fecha_reserva = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} reservó {self.clase.nombre}"
+
+
+# Create your models here.
